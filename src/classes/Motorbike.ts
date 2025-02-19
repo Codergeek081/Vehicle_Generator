c17736053ad4886fdab4cd28f7b4a331b34e86a7
Codergeek081 <issefarah15@gmail.com>
2025-02-19 08:13:27 -0500

    Added information to Motorbike and Vehcile classes

--- src/classes/Motorbike.ts
+++ src/classes/Motorbike.ts
@@ -2,24 +2,43 @@
 import Vehicle from './Vehicle.js';
 import Wheel from './Wheel.js';
 
-// TODO: The Motorbike class should extend the Vehicle class
-class Motorbike {
-  // TODO: Declare properties of the Motorbike class
-  // TODO: The properties should include vin, color, make, model, year, weight, top speed, and wheels
-  // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[])
+// The Motorbike class extends the Vehicle class
+class Motorbike extends Vehicle {
+  wheels: Wheel[];
 
-  // TODO: Create a constructor that accepts the properties of the Motorbike class
-    // TODO: The constructor should call the constructor of the parent class, Vehicle
-    // TODO: The constructor should initialize the properties of the Motorbike class
-    // TODO: The constructor should check if the wheels array has 2 elements and create 2 new default Wheel objects if it does not
+  // Constructor for the Motorbike class
+  constructor(
+    vin: string,
+    color: string,
+    make: string,
+    model: string,
+    year: number,
+    weight: number,
+    topSpeed: number,
+    wheels: Wheel[] = []
+  ) {
+    super(vin, color, make, model, year, weight, topSpeed);
 
-  // TODO: Implement the wheelie method
-    // TODO: The method should log the message "Motorbike [make] [model] is doing a wheelie!"
+    // Ensure the motorbike always has exactly 2 wheels
+    if (wheels.length === 0) {
+      this.wheels = [new Wheel(), new Wheel()];
+    } else if (wheels.length === 2) {
+      this.wheels = wheels;
+    } else {
+      throw new Error('A motorbike must have exactly 2 wheels.');
+    }
+  }
 
-  // TODO: Override the printDetails method from the Vehicle class
-  // TODO: The method should call the printDetails method of the parent class
-  // TODO: The method should log the details of the Motorbike
-  // TODO: The details should include the VIN, make, model, year, weight, top speed, color, and wheels
+  // Implementing the wheelie method
+  wheelie(): void {
+    console.log(`Motorbike ${this.make} ${this.model} is doing a wheelie! 🏍️`);
+  }
+
+  // Overriding the printDetails method from the Vehicle class
+  override printDetails(): void {  // <-- Added `override` here
+    super.printDetails();
+    console.log(`Wheels: ${this.wheels.length} wheels`);
+  }
 }
 
 // Export the Motorbike class as the default export
--- src/classes/Vehicle.ts
+++ src/classes/Vehicle.ts
@@ -1,22 +1,50 @@
-// import Driveable interface
+// Import Driveable interface
 import Driveable from '../interfaces/Driveable.js';
 
 // Vehicle class that implements Driveable interface
 class Vehicle implements Driveable {
-  // Declare properties of the Vehicle class
+  vin: string;
+  color: string;
+  make: string;
+  model: string;
+  year: number;
+  weight: number;
+  topSpeed: number;
   started: boolean;
   currentSpeed: number;
 
   // Constructor for the Vehicle class
-  constructor() {
-    this.started = false;
-    this.currentSpeed = 0;
+  constructor(
+    vin: string,
+    color: string,
+    make: string,
+    model: string,
+    year: number,
+    weight: number,
+    topSpeed: number
+  ) {
+    this.vin = vin;
+    this.color = color;
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.weight = weight;
+    this.topSpeed = topSpeed;
+    this.started = false; // Default value
+    this.currentSpeed = 0; // Default value
   }
 
   // Method to print vehicle details
   printDetails(): void {
+    console.log(`VIN: ${this.vin}`);
+    console.log(`Make: ${this.make}`);
+    console.log(`Model: ${this.model}`);
+    console.log(`Year: ${this.year}`);
+    console.log(`Weight: ${this.weight} kg`);
+    console.log(`Top Speed: ${this.topSpeed} km/h`);
+    console.log(`Color: ${this.color}`);
     console.log(`Vehicle started: ${this.started}`);
-    console.log(`Vehicle current speed: ${this.currentSpeed} mph`);
+    console.log(`Current speed: ${this.currentSpeed} mph`);
   }
 
   // Method to start the vehicle
@@ -27,7 +55,6 @@ class Vehicle implements Driveable {
 
   // Method to accelerate the vehicle
   accelerate(change: number): void {
-    // Check if the vehicle is started
     if (this.started) {
       this.currentSpeed += change;
       console.log(`Vehicle accelerated to ${this.currentSpeed} mph`);
@@ -38,7 +65,6 @@ class Vehicle implements Driveable {
 
   // Method to decelerate the vehicle
   decelerate(change: number): void {
-    // Check if the vehicle is started
     if (this.started) {
       this.currentSpeed -= change;
       console.log(`Vehicle decelerated to ${this.currentSpeed} mph`);
@@ -56,7 +82,6 @@ class Vehicle implements Driveable {
 
   // Method to turn the vehicle
   turn(direction: string): void {
-    // Check if the vehicle is started
     if (this.started) {
       console.log(`Vehicle turned ${direction}`);
     } else {
@@ -66,7 +91,6 @@ class Vehicle implements Driveable {
 
   // Method to reverse the vehicle
   reverse(): void {
-    // Check if the vehicle is started
     if (this.started) {
       console.log('Vehicle reversed');
     } else {
